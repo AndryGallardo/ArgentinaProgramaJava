ba83f7b9fd82a93da3ca70f8ec30eeec4e358c3a
Juan Huenz <juan.huenz@valtech.com>
2023-03-15 23:23:59 -0300

    Ejemplo del método anotado con @BeforeEach

--- TestUnitario/src/main/java/Calculadora.java
+++ TestUnitario/src/main/java/Calculadora.java
@@ -2,4 +2,8 @@ public class Calculadora {
     public double sumar(double valor1, double valor2) {
         return valor1 + valor2;
     }
+
+    public double resta(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
 }
--- TestUnitario/src/test/java/CalculadoraTest.java
+++ TestUnitario/src/test/java/CalculadoraTest.java
@@ -1,13 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class CalculadoraTest {
 
+    @BeforeEach
+    void antesDeCada() {
+        System.out.println("Este código se ejecuta antes de cada método anotado con @Test");
+    }
+
     @Test
     void sumaTest() {
         Calculadora calculadora = new Calculadora();
         double resultado = calculadora.sumar(2, 3);
         assertEquals(5, resultado);
+    }
+
+    @Test
+    void restaTest() {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.resta(2, 3);
+        assertEquals(-1, resultado);
     }
 }
